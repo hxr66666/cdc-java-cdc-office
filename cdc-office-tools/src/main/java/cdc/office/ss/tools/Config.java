ce2477535ff5ef192b1117479e19920e324990ff
Damien Carbonne <damien.carbonne@gmail.com>
2021-07-24 12:28:14 +0200

    Moved to next SNAPSHOT

--- cdc-office-tools/src/main/java/cdc/office/ss/tools/Config.java
+++ cdc-office-tools/src/main/java/cdc/office/ss/tools/Config.java
@@ -1,7 +1,7 @@
 package cdc.office.ss.tools;
 
 public final class Config {
-    public static final String VERSION = "0.11.0";
+    public static final String VERSION = "0.12.0-SNAPSHOT";
 
     private Config() {
     }
