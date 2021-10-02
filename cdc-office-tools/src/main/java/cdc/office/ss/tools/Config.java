6fe431d1beb946feba322f5caba6c68522981c7e
Damien Carbonne <damien.carbonne@gmail.com>
2021-10-02 18:08:07 +0200

    Prepare 0.12.0 Release

--- cdc-office-tools/src/main/java/cdc/office/ss/tools/Config.java
+++ cdc-office-tools/src/main/java/cdc/office/ss/tools/Config.java
@@ -1,7 +1,7 @@
 package cdc.office.ss.tools;
 
 public final class Config {
-    public static final String VERSION = "0.12.0-SNAPSHOT";
+    public static final String VERSION = "0.12.0";
 
     private Config() {
     }
