3283fec9b2b6dc0e63e9f4aa7ee4ef226ee9d3a6
Damien Carbonne <damien.carbonne@gmail.com>
2022-02-13 20:06:27 +0100

    Prepare 0.21.0 Release

--- cdc-office-tools/src/main/java/cdc/office/tools/Config.java
+++ cdc-office-tools/src/main/java/cdc/office/tools/Config.java
@@ -1,7 +1,7 @@
 package cdc.office.tools;
 
 public final class Config {
-    public static final String VERSION = "0.21.0-SNAPSHOT";
+    public static final String VERSION = "0.21.0";
 
     private Config() {
     }
