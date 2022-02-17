d0b3993ccd8c21de1b2f300baea530488e1a2533
Damien Carbonne <damien.carbonne@gmail.com>
2022-02-17 22:43:14 +0100

    Retrieve Config data from Manifest.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+### Changed
+- Updated dependencies:
+    - cdc-util-0.21.0
+- `Config` data is now retrieved from Manifest.
+
+
 ## [0.21.0] - 2022-02-13
 ### Added
 - Added `CsvWriter` constructors using `OutputStream`.  #15
--- cdc-office-tools/src/main/java/cdc/office/tools/Config.java
+++ cdc-office-tools/src/main/java/cdc/office/tools/Config.java
@@ -1,7 +1,17 @@
 package cdc.office.tools;
 
+import java.util.Optional;
+import java.util.jar.Manifest;
+
+import cdc.util.lang.ManifestUtils;
+
 public final class Config {
-    public static final String VERSION = "0.22.0-SNAPSHOT";
+    public static final String VERSION;
+
+    static {
+        final Optional<Manifest> manifest = ManifestUtils.getManifest(Config.class);
+        VERSION = ManifestUtils.getValue(manifest, ManifestUtils.IMPLEMENTATION_VERSION, "SNAPSHOT");
+    }
 
     private Config() {
     }
